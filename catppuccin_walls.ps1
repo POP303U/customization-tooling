3d2d548a4fccd6daa8108a483afab1c29c87fce5
POP303U <nicoanton.gering@gmail.com>
2023-10-26 21:12:28 +0200

    Add Brave, Scripts

---
+++ catppuccin_walls.ps1
@@ -0,0 +1,2 @@
+#arch_papers catppuccin/salty_mountains.png out.png --mask masks/rust.svg -s 3840,2160 -c 313244
+arch_papers catppuccin/salty_mountains.png out.png -s 3840,2160 -c 313244
---
+++ rose-pine_walls.ps1
@@ -0,0 +1,2 @@
+#arch_papers catppuccin/salty_mountains.png out.png --mask masks/rust.svg -s 3840,2160 -c 1f1d2e
+arch_papers catppuccin/salty_mountains.png out.png -s 3840,2160 -c 1f1d2e
